970a198bb3bcc6863f884bb76ed014757adb8206
AlvaroTHG <alvarobaldiviezo8@gmail.com>
2025-03-24 19:22:45 -0400

    Actividad Alvaro y Juan José

--- refactorizacionflutter.dart
+++ refactorizacionflutter.dart
@@ -4,43 +4,54 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var l = [];
-  var t = 0.0;
+  var productos = [];
+  var total = 0.0;
   
-  void a(String n, double p, int q) {
+
+  //Función que recopila la id, precio y cantidad para agregar un nuevo producto. 
+  void agregarProducto(String id, double precio, int cantidad) {
+
     setState(() {
-      l.add({"n": n, "p": p, "q": q});
-      c();
+      productos.add({"id": id, "precio": precio, "cantidad": cantidad});
+      calcularTotalProducto();
     });
+
   }
   
-  void c() {
-    t = 0;
-    for (var i = 0; i < l.length; i++) {
-      t += l[i]["p"] * l[i]["q"];
+  //Obtiene el precio y cantidad del producto en posición "i", para luego obtener el total.
+  void calcularTotalProducto() {
+    for (var i = 0; i < productos.length; i++) {
+      total += productos[i]["precio"] * productos[i]["cantidad"];
     }
   }
   
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Mi App")),
         body: Column(
           children: [
-            // Interfaz para agregar productos...
-            Text("Total: \$${t.toStringAsFixed(2)}"),
+            //INTERFAZ PARA MOSTRAR LOS PRODUCTOS
+            Text("Total: \$${total.toStringAsFixed(2)}"),
             Expanded(
+
               child: ListView.builder(
-                itemCount: l.length,
+
+                itemCount: productos.length,
                 itemBuilder: (context, index) {
+
                   return ListTile(
-                    title: Text(l[index]["n"]),
-                    subtitle: Text("Precio: \$${l[index]["p"]} x ${l[index]["q"]}"),
+
+                    title: Text(productos[index]["id"]),
+                    subtitle: Text("Precio: \$${productos[index]["precio"]} x ${productos[index]["cantidad"]}"),
+
                   );
                 },
               ),
             ),
+
           ],
         ),
       ),
